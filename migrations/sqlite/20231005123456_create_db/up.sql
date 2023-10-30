73346896cc6d0b5156b021eaed883eeec3cf46f8
neil <neil@noreply.example.org>
2023-10-30 15:12:25 +0100

    remove sqlite test entries

--- migrations/sqlite/20231005123456_create_db/up.sql
+++ migrations/sqlite/20231005123456_create_db/up.sql
@@ -22,15 +22,3 @@ CREATE TABLE "star" (
     position_y REAL NOT NULL,
     transactionid INTEGER NOT NULL REFERENCES "transaction"
 );
-
-INSERT INTO "transaction" (username, message, email, day, amount, gems, token, ha_id, receipt_url, event_type, is_mail_sent, is_token_used, is_checked) VALUES ('Brume', 'Coucouuuuu (｡◕‿◕｡)', 'brume@42l.fr', '2023-10-09', 1000, 40, 'blehtoken', 1, 'http://bleh', 'monthlydonation', false, true, false);
-INSERT INTO "transaction" (username, message, email, day, amount, gems, token, ha_id, receipt_url, event_type, is_mail_sent, is_token_used, is_checked) VALUES ('Neil', 'BLEHBLEHBLEH', 'neil@42l.fr', '2023-10-19', 1000, 100, 'blehtoken2', 2, 'http://bleh', 'monthlydonation', false, false, false);
-
-INSERT INTO star (startype, position_x, position_y, transactionid) VALUES (1, 95, 42, 1);
-INSERT INTO star (startype, position_x, position_y, transactionid) VALUES (2, 37, 62, 1);
-INSERT INTO star (startype, position_x, position_y, transactionid) VALUES (3, 18, 32, 1);
-INSERT INTO star (startype, position_x, position_y, transactionid) VALUES (4, 30, 42, 1);
-INSERT INTO star (startype, position_x, position_y, transactionid) VALUES (1, 15, 12, 2);
-INSERT INTO star (startype, position_x, position_y, transactionid) VALUES (2, 27, 12, 2);
-INSERT INTO star (startype, position_x, position_y, transactionid) VALUES (4, 11, 32, 2);
-INSERT INTO star (startype, position_x, position_y, transactionid) VALUES (1, 35, 72, 2);
