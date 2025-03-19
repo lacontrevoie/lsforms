6d620d56b5170ac302bea007edc20c587fa587e8
neil <neil@noreply.example.org>
2025-03-19 15:34:29 +0100

    add missing debug logs on mail sending ; log successfully sent messages

--- src/emails.rs
+++ src/emails.rs
@@ -1,12 +1,13 @@
-use crate::config::structs::{Config, HostConfig, HostInputKind};
+use crate::config::structs::{Config, HostConfig, HostInputKind, VerboseLevel};
 use crate::config::methods::load_templates;
-use crate::errors::{throw, ErrorKind as EK, ServerError};
+use crate::errors::{throw, ErrorKind as EK, ServerError, dbg_print_form};
 
 use lettre::address::AddressError;
 use lettre::message::header::ContentType;
 use lettre::transport::smtp::authentication::Credentials;
 use lettre::{Message, AsyncSmtpTransport, Tokio1Executor, AsyncTransport};
 use std::collections::HashMap;
+use chrono::Utc;
 
 
 pub async fn deliver(
@@ -18,13 +19,21 @@ pub async fn deliver(
 ) -> Result<(), ServerError> {
 
     let mail_body = load_templates().get(&host_config.language)
-        .ok_or_else(|| throw(EK::EmailLanguageNotFound, format!("defined language not found for {host_name} ({:?})", &host_config.language)))?
+        .ok_or_else(|| {
+            dbg_print_form(config, form_values.clone());
+            throw(EK::EmailLanguageNotFound, format!("defined language not found for {host_name} ({:?})", &host_config.language))
+        })?
         .to_string();
 
-    let form_data = fill_form_data(host_config, form_values)?;
+    let form_data = fill_form_data(host_config, form_values).inspect_err(|_| {
+        dbg_print_form(config, form_values.clone());
+    })?;
 
     let subject = mail_body.lines().next()
-        .ok_or_else(|| throw(EK::EmailLinesFail, format!("Could not read next line for {host_name}, {mail_body:?}")))?;
+        .ok_or_else(|| {
+            dbg_print_form(config, form_values.clone());
+            throw(EK::EmailLinesFail, format!("Could not read next line for {host_name}, {mail_body:?}"))
+        })?;
     
     let mail_body = mail_body
         .replace(subject, "")
@@ -43,16 +52,23 @@ pub async fn deliver(
         Message::builder()
             .from(
                 config.mail.send_as.parse().map_err(|e: AddressError| {
+                    dbg_print_form(config, form_values.clone());
                     throw(EK::EmailFromParseFail, e.to_string())
                 })?,
             )
             .to(host_config.recipient
                 .parse()
-                .map_err(|e: AddressError| throw(EK::EmailToParseFail, e.to_string()))?)
+                .map_err(|e: AddressError| {
+                    dbg_print_form(config, form_values.clone());
+                    throw(EK::EmailToParseFail, e.to_string())
+                })?)
             .subject(subject)
             .header(ContentType::TEXT_PLAIN)
             .body(mail_body)
-            .map_err(|e| throw(EK::EmailBodyParseFail, e.to_string()))?;
+            .map_err(|e| {
+                dbg_print_form(config, form_values.clone());
+                throw(EK::EmailBodyParseFail, e.to_string())
+            })?;
 
     // build the mail server credentials
     let creds = Credentials::new(
@@ -63,7 +79,10 @@ pub async fn deliver(
     // Open a remote connection to mail server
     let mailer: AsyncSmtpTransport<Tokio1Executor> =
         AsyncSmtpTransport::<Tokio1Executor>::relay(&config.mail.mailserver_address)
-            .map_err(|e| throw(EK::EmailHostnameReadFail, format!("Could not read mailserver hostname {e}")))?
+            .map_err(|e| {
+                dbg_print_form(config, form_values.clone());
+                throw(EK::EmailHostnameReadFail, format!("Could not read mailserver hostname {e}"))
+            })?
             .credentials(creds)
             .build();
     
@@ -71,78 +90,18 @@ pub async fn deliver(
     mailer
         .send(m)
         .await
-        .map_err(|e| throw(EK::EmailSendFail, e.to_string()))?;
+        .map_err(|e| {
+            dbg_print_form(config, form_values.clone());
+            throw(EK::EmailSendFail, e.to_string())
+        })?;
+
+    if config.general.verbose_level == VerboseLevel::Info {
+        println!("Form sent successfully to {} at {}", host_config.recipient, Utc::now().format("%+"));
+    }
 
     Ok(())
 }
 
-/*pub fn deliver(
-    config: &Config,
-    client_ip: &str,
-    host_name: &str,
-    host_config: &HostConfig,
-    form_values: &HashMap<String, String>
-) -> Result<(), ServerError> {
-    
-    let mail_body = load_templates().get(&host_config.language)
-        .ok_or_else(|| throw(EK::EmailLanguageNotFound, format!("defined language not found for {host_name} ({:?})", &host_config.language)))?
-        .to_string();
-
-    let form_data = fill_form_data(host_config, form_values)?;
-
-    let subject = mail_body.lines().next()
-        .ok_or_else(|| throw(EK::EmailLinesFail, format!("Could not read next line for {host_name}, {mail_body:?}")))?;
-    
-    let mail_body = mail_body
-        .replace(subject, "")
-        .replace("{{HOST}}", host_name)
-        .replace("{{IP}}", client_ip)
-        .replace("{{FORMDATA}}", &form_data)
-        .replace("{{CONTACT}}", &config.mail.contact_spam)
-        .replace("{{MAIL_SIGNATURE}}", &config.mail.mail_signature);
-
-    let subject = subject
-        .replace("{{SUBJECT}}", "")
-        .replace("{{/SUBJECT}}", "");
-
-
-    //mail_body = mail_body.replace("{{LINK}}", "aaa");
-    // build the message body and headers
-    let m =
-        Message::builder()
-            .from(
-                config.mail.send_as.parse().map_err(|e: AddressError| {
-                    throw(EK::EmailFromParseFail, e.to_string())
-                })?,
-            )
-            .to(host_config.recipient
-                .parse()
-                .map_err(|e: AddressError| throw(EK::EmailToParseFail, e.to_string()))?)
-            .subject(subject)
-            .header(ContentType::TEXT_PLAIN)
-            .body(mail_body)
-            .map_err(|e| throw(EK::EmailBodyParseFail, e.to_string()))?;
-
-    // build the mail server credentials
-    let creds = Credentials::new(
-        config.mail.sender_email.clone(),
-        config.mail.sender_password.clone(),
-    );
-
-    // Open a remote connection to mail server
-    let mailer = SmtpTransport::relay(&config.mail.mailserver_address)
-        .map_err(|e| throw(EK::EmailHostnameReadFail, format!("Could not read mailserver hostname {e}")))?
-        .credentials(creds)
-        .build();
-
-    // Send the email
-    mailer
-        .send(&m)
-        .map_err(|e| throw(EK::EmailSendFail, e.to_string()))?;
-
-    Ok(())
-}*/
-    
 fn fill_form_data(host_config: &HostConfig, form_values: &HashMap<String, String>) -> Result<String, ServerError> {
     let mut form_data = String::new();
     for field in &host_config.inputs {
@@ -168,5 +127,6 @@ fn fill_form_data(host_config: &HostConfig, form_values: &HashMap<String, String
             form_data.push_str(&format!("{}: {}\n\n", field.display_name, field_value));
         }
     }
+
     Ok(form_data)
 }
